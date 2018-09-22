92fdd9250693c7ed835dd7647f7d4959370ef136
HIJRATULGEBI PRAMANA <42725375+HijratulgebiPramana@users.noreply.github.com>
2018-09-22 11:02:24 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1 style=" padding: 5px; background-color: red; color: white; text-align: center;">header<br><a href="form.php"><font color="white">form</font></a>
+<a href="login.php"><font color="white">Home</font></a></h1>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+</head>
+<body>
+	<form action="log-1.php" method="post">
+		<center><h3>Selamat datang Di Website DPS</h3>
+				<h3>DPS Melayani Konsumen dengan Sepenuh Hati </h3></center>
+
+		<table style="margin-top: 3%" align="center">
+			<tr>
+				<td>username</td>
+				<td>:</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>password</td>
+				<td>:</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td align="center" colspan="3"><input type="submit" value="Login"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
